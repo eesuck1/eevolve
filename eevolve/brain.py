4b660038f8a5a7bd7d3986031cd33cb0200c1f0d
Isak Volodymyr (CSS SRI ASR 1) <volodymyr.isak@infineon.com>
2024-11-07 16:00:01 +0200

    PyTorch removed by now

--- eevolve/brain.py
+++ eevolve/brain.py
@@ -1,63 +1,25 @@
 from typing import Iterable, Any, Callable
 
-import torch
 import numpy
 
 
 class Brain:
-    def __init__(self, mapping: Iterable[Any] | dict[float | int, Any] | Callable, learning_rate: float = 1e-3):
+    def __init__(self, mapping: Iterable[Any] | dict[float | int, Any] | Callable):
         self._mapping = mapping
         self._output = None
         self._decision = None
 
-        self._model = torch.nn.Sequential()
-        self._metric = torch.Tensor([0.0])
-        self._input_tensor = torch.Tensor([0.0])
+        self._weights = None
 
-        self._learning_rate = learning_rate
-        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=learning_rate)
+    def add_layer(self) -> None:
+        ...
 
-    def add_layer(self, layer: torch.nn.Module, name: str = None) -> None:
-        if not isinstance(layer, torch.nn.Module):
-            raise ValueError(f"Layer should be of `torch.nn.Module` type. {type(layer)} passed instead!")
+    def add_layers(self) -> None:
+        ...
 
-        if name is None:
-            name = f"{layer.__class__.__name__}_{len(self._model)}"
-
-        self._model.add_module(name, layer)
-        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=self._learning_rate)
-
-    def add_layers(self, layers: Iterable[torch.nn.Module] | Any, names: Iterable[str] | Any = None) -> None:
-        if names is None:
-            names = [None for _ in range(len(layers))]
-        elif len(layers) != len(names):
-            raise ValueError(f"`layers` length must be equal `names` lengths."
-                             f" {len(layers)} and {len(names)} passed instead!")
-
-        for layer, name in zip(layers, names):
-            self.add_layer(layer, name)
-
-    def forward(self, observation: Iterable[Any] | torch.Tensor | Any, owner: Any,
-                output_function: Callable[[torch.Tensor], Iterable[float | int] | numpy.ndarray | float | int | Any],
+    def forward(self, observation: Iterable[Any] | numpy.ndarray | Any, owner: Any = None,
                 *args, **kwargs) -> None:
-        if isinstance(observation, torch.Tensor):
-            self._input_tensor = observation
-        else:
-            self._input_tensor = torch.from_numpy(numpy.array(observation, dtype=numpy.float32))
-
-        self._output = output_function(self._model(self._input_tensor, *args, **kwargs))
-
-    def judge(self, value: float | torch.Tensor) -> None:
-        if isinstance(value, torch.Tensor):
-            self._metric -= value
-        else:
-            self._metric -= torch.Tensor(value)
-
-    def backward(self) -> None:
-        self._metric.backward()
-
-        self._optimizer.step()
-        self._optimizer.zero_grad()
+        ...
 
     def decide(self) -> Any:
         if self._mapping is None or not self._mapping:
@@ -77,9 +39,5 @@ class Brain:
         return self._decision
 
     @property
-    def model(self) -> torch.nn.Module:
-        return self._model
-
-    @model.setter
-    def model(self, model: torch.nn.Module):
-        self._model = model
+    def output(self) -> Any:
+        return self._output
--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="eevolve",
-    version="0.0.9",
+    version="0.0.10",
     author="Isak Volodymyr",
     author_email="volodymyr.o.isak@gmail.com",
     description="Evolution Algorithms Playground",
     packages=find_packages(),
     install_requires=[
-        "torch==2.5.1",
         "numpy==2.1.2",
         "pygame-ce==2.5.2",
     ]
