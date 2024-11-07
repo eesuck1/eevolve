ac6532fb12aab6d456bf5d693ba35f3a9d10a2ea
eesuck1 <eesucksc@gmail.com>
2024-11-07 13:38:02 +0200

    brain works

--- eevolve/brain.py
+++ eevolve/brain.py
@@ -5,13 +5,17 @@ import numpy
 
 
 class Brain:
-    def __init__(self, mapping: Iterable[Any] | dict[float | int, Any] | Callable):
+    def __init__(self, mapping: Iterable[Any] | dict[float | int, Any] | Callable, learning_rate: float = 1e-3):
         self._mapping = mapping
         self._output = None
         self._decision = None
 
         self._model = torch.nn.Sequential()
-        self._metric = torch.Tensor(0.0)
+        self._metric = torch.Tensor([0.0])
+        self._input_tensor = torch.Tensor([0.0])
+
+        self._learning_rate = learning_rate
+        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=learning_rate)
 
     def add_layer(self, layer: torch.nn.Module, name: str = None) -> None:
         if not isinstance(layer, torch.nn.Module):
@@ -21,6 +25,7 @@ class Brain:
             name = f"{layer.__class__.__name__}_{len(self._model)}"
 
         self._model.add_module(name, layer)
+        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=self._learning_rate)
 
     def add_layers(self, layers: Iterable[torch.nn.Module] | Any, names: Iterable[str] | Any = None) -> None:
         if names is None:
@@ -32,21 +37,34 @@ class Brain:
         for layer, name in zip(layers, names):
             self.add_layer(layer, name)
 
-    def forward(self, observation: Iterable[Any] | Any, owner: Any, *args, **kwargs) -> None:
-        self._output = self._model([float(value) for value in observation])
+    def forward(self, observation: Iterable[Any] | torch.Tensor | Any, owner: Any,
+                output_function: Callable[[torch.Tensor], Iterable[float | int] | numpy.ndarray | float | int | Any],
+                *args, **kwargs) -> None:
+        if isinstance(observation, torch.Tensor):
+            self._input_tensor = observation
+        else:
+            self._input_tensor = torch.from_numpy(numpy.array(observation, dtype=numpy.float32))
 
-    def metric(self) -> None:
-        pass
+        self._output = output_function(self._model(self._input_tensor, *args, **kwargs))
+
+    def judge(self, value: float | torch.Tensor) -> None:
+        if isinstance(value, torch.Tensor):
+            self._metric -= value
+        else:
+            self._metric -= torch.Tensor(value)
 
     def backward(self) -> None:
-        pass
+        self._metric.backward()
+
+        self._optimizer.step()
+        self._optimizer.zero_grad()
 
     def decide(self) -> Any:
         if self._mapping is None or not self._mapping:
             return None
 
         if isinstance(self._mapping, (list, tuple, numpy.ndarray)):
-            return self._mapping[self._output]
+            return self._mapping[int(self._output)]
         elif isinstance(self._mapping, dict):
             return self._mapping.get(self._output)
         elif callable(self._mapping):
